9344cf9056672cd8cc95fe04aaa93d7f6993c971
Ishan <ishanwijes@gmail.com>
2023-04-27 16:46:14 +1000

    added CRU functionality

--- nextjs-todo-app/components/Footer.js
+++ nextjs-todo-app/components/Footer.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='flex justify-center items-center gap-3 py-3'>
+      <a href="https://www.instagram.com/">
+        <i className="fa-brands fa-instagram duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/in/ishan-wijesingha-497222239/">
+        <i className="fa-brands fa-linkedin duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+
+      <a href="https://github.com/Ishan-Wijesingha99">
+        <i className="fa-brands fa-github duration-300 hover:opacity-30 cursor-pointer"></i>
+      </a>
+    </div>
   )
 }
